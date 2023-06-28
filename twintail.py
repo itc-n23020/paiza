58f58e766958a7b6e19ee4318d74789d721cc55d
haruru nakazato <n23020@std.it-college.ac.jp>
2023-06-28 07:39:44 +0000

    twintail5

--- twintail.py
+++ twintail.py
@@ -1,10 +1,15 @@
+def compare_performance(c1, p1, c2, p2):
+    performance1 = c1 / p1
+    performance2 = c2 / p2
+
+    if performance1 > performance2:
+        return 1
+    else:
+        return 2
+
+
 c1, p1 = map(int, input().split())
 c2, p2 = map(int, input().split())
 
-performance1 = c1 / p1
-performance2 = c2 / p2
-
-if performance1 > performance2:
-    print(1)
-else:
-    print(2)
+result = compare_performance(c1, p1, c2, p2)
+print(result)
