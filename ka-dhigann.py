dae90e99b84e925d017b60dd3f64c60187c5e686
haruru nakazato <n23020@std.it-college.ac.jp>
2023-06-28 07:35:02 +0000

    ka-dhigann5

--- ka-dhigann.py
+++ ka-dhigann.py
@@ -1,7 +1,10 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
 n = int(input())
-
-result = 1
-for i in range(1, n + 1):
-    result *= i
-
+result = factorial(n)
 print(result)
