6a9a9fe7eefccddc305b240090a592f960803f25
haruru nakazato <n23020@std.it-college.ac.jp>
2023-06-28 07:36:38 +0000

    longhair5

--- longhair.py
+++ longhair.py
@@ -1,10 +1,12 @@
-# 入力の受け取りとカウント
-votes = [input() for _ in range(5)]
-yes_count = votes.count("yes")
-no_count = votes.count("no")
+def count_votes(votes):
+    yes_count = votes.count("yes")
+    no_count = votes.count("no")
+    if yes_count > no_count:
+        return "yes"
+    else:
+        return "no"
 
-# より多い票数の方を出力
-if yes_count > no_count:
-    print("yes")
-else:
-    print("no")
+
+votes = [input() for _ in range(5)]
+result = count_votes(votes)
+print(result)
